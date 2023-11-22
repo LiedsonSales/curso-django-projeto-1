363267362dfcf15e4d2b063a107694f0fc9f03df
Liedson Sales <liedson.r.sales@gmail.com>
2023-11-22 20:05:09 -0300

    removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name':'Liedson Sales'
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-def globall(request):
-    return render(request, 'global/home.html', status=404)
 
 # Create your views here.
